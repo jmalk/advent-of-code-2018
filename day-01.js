5b2ce3469f0f5eac0138c9a67ecbd2e4c2d2d8dd
Joshua Malkinson <joshua.malkinson@bbc.co.uk>
2018-12-12 13:09:22 +0000

    Day one part two solution

--- day-01.js
+++ day-01.js
@@ -3,8 +3,33 @@ const fs = require('fs');
 fs.readFile('day-01-input', 'utf8', (err, data) => {
   if (err) throw err;
   const frequencyChanges = data.split('\n')
-    .map(change => parseInt(change));
-  console.log(frequencyChanges);
+  .map(change => parseInt(change));
+
   const frequencyDiff = frequencyChanges.reduce((a, b) => a + b);
-  console.log(frequencyDiff);
+  console.log(`Result of all frequency changes: ${frequencyDiff}`);
+
+  const firstFrequencyReachedTwice = calculateFirstFrequencyReachedTwice(frequencyChanges);
+  console.log(`First frequency reached twice: ${firstFrequencyReachedTwice}`);
 });
+
+function calculateFirstFrequencyReachedTwice (frequencyChanges) {
+  let frequenciesReached = [0];
+  let firstRepeat;
+
+  for (let limitCount = 0; limitCount < 10000; limitCount++) {
+    if (firstRepeat !== undefined) {
+      break;
+    }
+    for (let i = 0; i < frequencyChanges.length; i++) {
+      let newFrequency = frequenciesReached[frequenciesReached.length - 1] + frequencyChanges[i];
+      if (frequenciesReached.includes(newFrequency)) {
+        firstRepeat = newFrequency;
+        break;
+      } else {
+        frequenciesReached.push(newFrequency);
+      }
+    }
+  }
+
+  return firstRepeat;
+}

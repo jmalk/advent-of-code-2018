184d68ab2e94b5b7a55a519d5d0bfe208c4691c9
Joshua Malkinson <joshua.malkinson@bbc.co.uk>
2018-12-12 16:10:17 +0000

    Day two part one solution

--- day-02.js
+++ day-02.js
@@ -4,8 +4,67 @@ let readFile = readFilePromise('./inputs/day-02');
 
 readFile
   .then((data) => {
-    console.log(data);
+    let boxIds = data.split('\n');
+
+    function reducer (tallies, currentValue) {
+      if (aLetterOccursExactlyNTimes(currentValue, 2)) {
+        tallies.idsWithExactlyTwoOfALetter += 1;
+      }
+      if (aLetterOccursExactlyNTimes(currentValue, 3)) {
+        tallies.idsWithExactlyThreeOfALetter += 1;
+      }
+      return tallies;
+    }
+
+    let startingTallies = {
+      idsWithExactlyTwoOfALetter: 0,
+      idsWithExactlyThreeOfALetter: 0
+    };
+
+    let finalTallies = boxIds.reduce(reducer, startingTallies);
+    console.log({finalTallies});
+
+    const rudimentaryHash = finalTallies.idsWithExactlyTwoOfALetter * finalTallies.idsWithExactlyThreeOfALetter;
+    console.log({rudimentaryHash});
   })
   .catch((err) => {
     console.error(err);
   });
+
+function aLetterOccursExactlyNTimes (string, n) {
+  const letterRegExes = [
+    /a/g,
+    /b/g,
+    /c/g,
+    /d/g,
+    /e/g,
+    /f/g,
+    /g/g,
+    /h/g,
+    /i/g,
+    /j/g,
+    /k/g,
+    /l/g,
+    /m/g,
+    /n/g,
+    /o/g,
+    /p/g,
+    /q/g,
+    /r/g,
+    /s/g,
+    /t/g,
+    /u/g,
+    /v/g,
+    /w/g,
+    /x/g,
+    /y/g,
+    /z/g,
+  ];
+
+  const lettersWhichAppearTwice = letterRegExes.filter((letterRegEx) => {
+    const matches = string.match(letterRegEx);
+    return matches && matches.length === n;
+  })
+
+  return lettersWhichAppearTwice.length > 0;
+}
